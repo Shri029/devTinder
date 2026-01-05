f70203a99877a12d3be777cadb2c7c84e1539078
Shivani Tiwary <30630384+Shri029@users.noreply.github.com>
2026-01-05 19:13:40 +0530

    Init database

--- src/app.js
+++ src/app.js
@@ -1,18 +1,39 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
 const app= express();
+const User = require("./models/user");
 
-app.use((req, res) =>{
-    res.send("Hello ");
+
+app.post("/signup", async (req,res) =>{
+    const user = new User({
+        firstName : "Virat",
+        lastName : "Kohli",
+        emailId : "Virat@Kohli.com",
+        password : "virat@123",
+    });
+
+    // creating new instance of the suer model
+    // const user = new User(userObj);
+    try{
+        await user.save();
+        res.send("User added successfully");
+    }catch (err) {
+        res.status(400).send("Error ssving the user: "+err.message);
+    }
+
 });
 
-app.use("/test", (req, res) =>{
-    res.send("Hello from the server");
-});
-
-app.listen(3000, () => {
-    console.log("Server is successfully listening to port 3000...");
+connectDB()
+    .then(() => {
+        console.log("Database connection established...");
+        app.listen(3000, () => {
+            console.log("Server is successfully listening to port 3000...");
+        });
+}).catch((err)=>{
+    console.log("Database can not be connected");
 })
+
+
 
 
  
