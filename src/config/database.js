6ba3cae89396929a46f28f03149c3a049c90c07c
Shivani Tiwary <30630384+Shri029@users.noreply.github.com>
2026-01-05 19:16:49 +0530

    Revert "Init database"
    This reverts commit f70203a99877a12d3be777cadb2c7c84e1539078.

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-    await mongoose.connect(
-    "mongodb+srv://MongoDB:P18kuu8ZRe3NRmaR@mongodb.hvlvvmz.mongodb.net/devTinder"
-);
-}
-
-module.exports = connectDB;
